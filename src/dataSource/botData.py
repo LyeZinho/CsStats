131eaa89fac9b5aac84d316f7adba8d1cfdb0754
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-01-19 23:51:13 +0000

    Changes

--- src/dataSource/botData.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def newBotTable():
-  conect = sqlite3.connect("src/botdata.db")
-  cursor = conect.cursor()
-  table = "CREATE TABLE IF NOT EXISTS Bot (id, token)" 
-  cursor.execute(table)
-  conect.commit()
-  conect.close()
